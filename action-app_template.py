cc38d80368824ae368251099497ff198ee801a1e
Ben <benthegarvin@gmail.com>
2019-04-20 14:43:20 -0700

    Scheduled downloads test

--- action-app_template.py
+++ action-app_template.py
@@ -7,6 +7,12 @@ from hermes_python.ontology import *
 import io
 import urllib2
 import json
+import schedule
+from threading import Thread
+import time
+import math
+from datetime import datetime, timedelta
+import calendar
 
 
 CONFIG_INI = "config.ini"
@@ -44,13 +50,27 @@ class Weather(object):
         print '[Received] intent: {}'.format(intent_message.intent.intent_name)
 
         parsed = self.get_weather()
+        current_time = int(time.time())
+        sentence = 'An error occured when getting the weather.'
+        if(current_time - parsed['currently']['time'] < 60):
+            sentence = 'It is {} outside, with a temperature of {} degrees.'.format(
+                parsed['currently']['summary'], parsed['currently']['temperature'])
+        elif(current_time - parsed['currently']['time'] < 172800):  # 48 hours
+            for i in parsed['hourly']['data']:
+                if(i['time'] == current_time-(current_time % 3600)):
+                    sentence = 'It is {} outside, with a temperature of {} degrees. This hourly data was gathered {}. For more up-to-date information, connect to the internet.'.format(
+                        i['summary'], i['temperature'], self.get_readable_date(i['time']))
+                break
+        else:
+            epoch_today_morning = int((datetime.now().replace(hour=7, minute=0, second=0, microsecond=0) - datetime.utcfromtimestamp(0)).total_seconds())
+            for i in parsed['daily']['data']:
+                if(i['time'] == epoch_today_morning):
+                    sentence = 'Today, it is supposed to be {} outside, with high of {} and a low of {}. This daily data was gathered {}. For more up-to-date information, connect to the internet.'.format(
+                i['summary'], i['temperatureHigh'], i['temperatureLow'], self.get_readable_date(i['time']))
 
-        sentence = 'It is {} outside, with a temperature of {} degrees.'.format(
-            parsed['currently']['summary'], parsed['currently']['temperature'])
 
-        # if need to speak the execution result by tts
         hermes.publish_start_session_notification(
-            intent_message.site_id, sentence, "")
+                intent_message.site_id, sentence, "")
 
     def temperature_callback(self, hermes, intent_message):
         # terminate the session first if not continue
@@ -97,7 +117,32 @@ class Weather(object):
         except urllib2.URLError:
             with open('weather.json', 'r') as cache:
                 return json.loads(cache.read())
+    def get_readable_date(self, time_input):
+        time_delta = time.time() - time_input
+        current_time = time.localtime()
+        current_datetime = datetime.now()
+        parsed_time = time.localtime(time_input)
+        parsed_datetime = datetime.fromtimestamp(time_input)
+        yesterday_morning_datetime = (
+            current_datetime - timedelta(days=1)).replace(hour=0, minute=0, second=0, microsecond=0)
+        print(yesterday_morning_datetime)
+        if(parsed_datetime > yesterday_morning_datetime):  # two days
+            if(parsed_time.tm_mday == current_time.tm_mday):
+                return time.strftime('today at %-I %p', parsed_time)
+            return time.strftime('yesterday at %-I %p', parsed_time)
+        else:
+            if(math.ceil(time_delta/172800)) > 1:
+                unit = 'days'
+            else:
+                unit = 'day'
+            return '{} {} ago'.format(int(math.ceil(time_delta/172800)), unit)
+
+
+def auto_download_weather(weather):
+    schedule.every().day.at("6:00").do(weather.download_weather)
 
 
 if __name__ == "__main__":
-    Weather()
+    weather = Weather()
+    thread = Thread(target=auto_download_weather, args=(weather))
+    thread.start()
